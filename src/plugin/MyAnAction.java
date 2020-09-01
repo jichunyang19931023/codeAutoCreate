63451636a6802edac18512733ded2d529c163d5e
jichunyang <jichunyang@int-yt.com>
2020-09-01 18:07:54 +0800

    添加目录层次遍历和错误捕获提示

--- src/plugin/MyAnAction.java
+++ src/plugin/MyAnAction.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.*;
 import org.jetbrains.annotations.NotNull;
 import java.util.Map;
@@ -27,18 +28,35 @@ public class MyAnAction extends AnAction {
             return;
         }
 
-        // 获取当前实体所在目录的上级目录，需要严格按说明中的目录组织，其它目录不考虑
-        workDir = aClass.getContainingFile().getContainingDirectory().getParentDirectory();
+        // 获取当前实体所在目录
+        PsiDirectory psiDirectory = aClass.getContainingFile().getContainingDirectory();
+        // 目录层次不得大于5
+        for (int i = 0; i < 3; i++){
+            if (psiDirectory.getName().contains("entity") || psiDirectory.getName().contains("domain")){
+                workDir = psiDirectory.getParentDirectory();
+                break;
+            }
+            psiDirectory = psiDirectory.getParentDirectory();
+        }
+        if (workDir == null){
+            Messages.showMessageDialog(project, "entity所在目录层次不得大于3", "Plugin Error", Messages.getErrorIcon());
+        }
+
         Map<String, String> configMap = ConfigDispose.getConfigPath(project, aClass);
 
-        // 自动创建服务文件
-        ClassCreator.createServiceFile(workDir, project, aClass, configMap);
-        // 自动创建服务实现文件
-        ClassCreator.createServiceImplFile(workDir, project, aClass, configMap);
-        // 自动创建控制器文件
-        ClassCreator.createControllerFile(workDir, project, aClass, configMap);
-        // 自动创建repository文件
-        ClassCreator.createRepositoryFile(workDir, project, aClass, configMap);
+        try{
+            // 自动创建服务文件
+            ClassCreator.createServiceFile(workDir, project, aClass, configMap);
+            // 自动创建服务实现文件
+            ClassCreator.createServiceImplFile(workDir, project, aClass, configMap);
+            // 自动创建控制器文件
+            ClassCreator.createControllerFile(workDir, project, aClass, configMap);
+            // 自动创建repository文件
+            ClassCreator.createRepositoryFile(workDir, project, aClass, configMap);
+            Messages.showMessageDialog(project, "全部文件创建成功！", "Plugin Success", Messages.getInformationIcon());
+        } catch (Exception e){
+            Messages.showMessageDialog(project, e.getMessage(), "Plugin Error", Messages.getErrorIcon());
+        }
     }
 
     public PsiClass getEditingClass(AnActionEvent anActionEvent) {
